722952294f63a7f4866c29eb24e7bd26178b720d
Scotty <jesse.scott.smith@ryerson.ca>
2022-02-15 20:28:23 -0500

    update socket server

--- socket.js
+++ socket.js
@@ -1,6 +1,9 @@
+const http = require('http')
 const express = require('express')
 const PORT = process.env.PORT || 8000
-const server = express()
+const app = express()
+
+var server = http.createServer(app);
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
